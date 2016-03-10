5692caefdd46a43d682f4f113fb2eab85bc77ac6
somkiat <somkiat.p@gmail.com>
2016-03-10 09:10:01 +0700

    Workshop UI Testing

---
+++ demo/UI-Testing-Cheat-Sheet/App/Controllers/ManageRosterTableViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ManageRosterTableViewController.swift
+//  UI Testing Cheat Sheet
+//
+//  Created by Joe Masilotti on 9/10/15.
+//  Copyright © 2015 Masilotti.com. All rights reserved.
+//
+
+import UIKit
+
+class ManageRosterTableViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.editing = true
+
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(ManageRosterTableViewController.refresh), forControlEvents: UIControlEvents.ValueChanged)
+        self.tableView.addSubview(refreshControl)
+        self.refreshControl = refreshControl
+    }
+
+    func refresh() {
+        let alert = UIAlertController(title: "Roster Refreshed", message: nil, preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Cancel, handler: nil))
+        presentViewController(alert, animated: true, completion: nil)
+
+        refreshControl!.endRefreshing()
+    }
+
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+
+    override func tableView(tableView: UITableView, editingStyleForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCellEditingStyle {
+        return UITableViewCellEditingStyle.None
+    }
+
+    override func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) { }
+}
---
+++ demo/UI-Testing-Cheat-Sheet/UITests/UITests.swift
@@ -0,0 +1,153 @@
+//
+//  UITests.swift
+//  UITests
+//
+//  Created by Joe Masilotti on 9/7/15.
+//  Copyright © 2015 Masilotti.com. All rights reserved.
+//
+
+import XCTest
+
+class UITests: UITestCase {
+    func testRefreshControl() {
+        app.staticTexts["Manage Roster"].tap()
+
+        let firstCell = app.staticTexts["Adrienne"]
+        let coordinate = firstCell.coordinateWithNormalizedOffset(CGVectorMake(0, 0))
+        let bottom = firstCell.coordinateWithNormalizedOffset(CGVectorMake(0, 6))
+        coordinate.pressForDuration(0, thenDragToCoordinate: bottom)
+
+        app.buttons["Dismiss"].tap()
+    }
+
+    func testElementExists() {
+        XCTAssert(app.staticTexts["Volley"].exists)
+    }
+
+    func testElementExistsWithPredicate() {
+        let predicate = NSPredicate(format: "label BEGINSWITH[cd] 'set your team details'")
+        let label = app.staticTexts.elementMatchingPredicate(predicate)
+        XCTAssert(label.exists)
+    }
+
+    func testElementWithEllipseExists() {
+        app.staticTexts["Manage Roster"].tap()
+
+        let longNameCell = app.staticTexts["Adolph Blaine Charles David Earl Frederick Gerald Hubert Irvin John Kenneth Lloyd Martin Nero Oliver Paul Quincy Randolph Sherman Thomas Uncas Victor William Xerxes Yancy Wolfeschlegelsteinhausenbergerdorff, Senior"]
+        XCTAssert(longNameCell.exists)
+    }
+
+    func testTappingAButton() {
+        app.buttons["More Info"].tap()
+        XCTAssert(app.navigationBars["Volleyball?"].exists)
+    }
+
+    func testTypingText() {
+        app.staticTexts["Manage Team"].tap()
+
+        let textField = app.textFields["Team Name"]
+        textField.tap()
+        textField.typeText("Dig Newtons")
+    }
+
+    func testDismissingAnAlert() {
+        app.staticTexts["View Schedule"].tap()
+
+        app.buttons["Finish Game"].tap()
+        app.alerts["You won!"].buttons["Awesome!"].tap()
+    }
+
+    func testDismissingASystemAlert() {
+        app.staticTexts["View Schedule"].tap()
+
+        addUIInterruptionMonitorWithDescription("Location Services") { (alert) -> Bool in
+            alert.buttons["Allow"].tap()
+            return true
+        }
+
+        app.buttons["Find Games Nearby?"].tap()
+        app.tap() // need to interact with the app for the handler to fire
+        XCTAssert(app.staticTexts["Authorized"].exists)
+    }
+
+    func testAdjustingASlider() {
+        app.staticTexts["Manage Team"].tap()
+
+        app.sliders.element.adjustToNormalizedSliderPosition(0.7)
+        XCTAssert(app.staticTexts["7"].exists)
+    }
+
+    func testAdjustingAPicker() {
+        app.staticTexts["Manage Team"].tap()
+
+        let selectedFormationLabel = app.staticTexts["5 attackers, 1 setter"]
+        XCTAssertFalse(selectedFormationLabel.exists)
+
+        let attackersPredicate = NSPredicate(format: "label BEGINSWITH 'Attackers Formation'")
+        let attackersPicker = app.pickerWheels.elementMatchingPredicate(attackersPredicate)
+        attackersPicker.adjustToPickerWheelValue("5 attackers")
+
+        let settersPredicate = NSPredicate(format: "label BEGINSWITH 'Setters Formation'")
+        let settersPicker = app.pickerWheels.elementMatchingPredicate(settersPredicate)
+        settersPicker.adjustToPickerWheelValue("1 setter")
+
+        XCTAssert(selectedFormationLabel.exists)
+    }
+
+    func testWaitingForAnElementToAppear() {
+        app.staticTexts["View Schedule"].tap()
+
+        let nextGameLabel = self.app.staticTexts["Game 4 - Tomorrow"]
+        let existsPredicate = NSPredicate(format: "exists == 1")
+        expectationForPredicate(existsPredicate, evaluatedWithObject: nextGameLabel, handler: nil)
+
+        app.buttons["Load More Games"].tap()
+
+        waitForExpectationsWithTimeout(5, handler: nil)
+        XCTAssert(nextGameLabel.exists)
+    }
+
+    func testCellReordering() {
+        app.staticTexts["Manage Roster"].tap()
+
+        let joeButton = app.buttons["Reorder Joe"]
+        let brianButton = app.buttons["Reorder Brian"]
+        joeButton.pressForDuration(0.5, thenDragToElement: brianButton)
+
+        XCTAssertLessThanOrEqual(joeButton.frame.maxY, brianButton.frame.minY)
+    }
+
+    func testTextExistsInAWebView() {
+        app.buttons["More Info"].tap()
+
+        let volleyballLabel = app.staticTexts["Volleyball"]
+        waitForElementToAppear(volleyballLabel)
+        XCTAssert(volleyballLabel.exists)
+    }
+
+    func testTappingALinkInAWebView() {
+        app.buttons["More Info"].tap()
+
+        let disambiguationLink = app.links["Volleyball (disambiguation)"]
+        waitForElementToAppear(disambiguationLink)
+        XCTAssert(disambiguationLink.exists)
+
+        disambiguationLink.tap()
+
+        let volleyballLink = app.links["Volleyball (ball)"]
+        waitForElementToAppear(volleyballLink)
+        XCTAssert(volleyballLink.exists)
+    }
+
+    func testPushingAController() {
+        app.buttons["More Info"].tap()
+        XCTAssert(app.navigationBars["Volleyball?"].exists)
+    }
+    
+    func testPoppingAViewController() {
+        app.buttons["More Info"].tap()
+        XCTAssert(app.navigationBars["Volleyball?"].exists)
+        app.navigationBars.buttons.elementBoundByIndex(0).tap()
+        XCTAssert(app.navigationBars["Volley"].exists)
+    }
+}
